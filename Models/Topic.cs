47686e0c493901531fe748599504b3dd430205b0
Tanaka Mawere <tanakamawere15@gmail.com>
2023-02-08 10:30:26 +0200

    Add project files.

---
+++ Models/Topic.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MedbaseBlazor.Models
+{
+    public class Topic
+    {
+        public long Id { get; set; }
+        [Required(ErrorMessage = "Select a course reference")]
+        public string CourseRef { get; set; }
+        [Required(ErrorMessage = "Enter a topic reference")]
+        public int TopicRef { get; set; }
+        [Required(ErrorMessage = "Enter a course name")]
+        public string Name { get; set; }
+    }
+}
---
+++ Services/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace MedbaseBlazor.Services
+{
+    public class Constants
+    {
+        public static double WeeklyAmount() => 1500;
+        public static double MonthlyAmount() => 3500;
+    }
+}
