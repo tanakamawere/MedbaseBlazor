2259b501d428e8db89805fce8af0219bee60e01f
Tanaka Mawere <tanakamawere15@gmail.com>
2023-02-08 10:30:26 +0200

    Add project files.

---
+++ Models/QuizQuestion.cs
@@ -0,0 +1,9 @@
+﻿namespace MedbaseBlazor.Models
+{
+    public class QuizQuestion
+    {
+        public string Question { get; set; }
+        public bool? Answer { get; set; }
+        public string Explanation { get; set; }
+    }
+}
