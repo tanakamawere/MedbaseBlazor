12553e6918e7a75cda53b01f0eba9b401d87481e
Tanaka Mawere <tanakamawere15@gmail.com>
2023-02-13 12:09:04 +0200

    added admin layout and admin subscriptions editing. Added changelog

--- Services/Constants.cs
+++ Services/Constants.cs
@@ -4,5 +4,11 @@
     {
         public static double WeeklyAmount() => 1500;
         public static double MonthlyAmount() => 3500;
+
+        public static Dictionary<string, int> SearchCategories = new()
+        {
+            {"Search by Id", 1},
+            {"Search by Keywords", 2 }
+        };
     }
 }
