b228d2ddc251b1c8740d99866b6d13a0eae1e6c6
Tanaka Mawere <tanaka@tanakamawere.co.zw>
2024-04-01 19:59:12 +0200

    well, sign in and up with JWT carrying claims is now working-ish

---
+++ Services/JwtCache.cs
@@ -0,0 +1,35 @@
+﻿using MedbaseLibrary.Services;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace MedbaseBlazor.Services;
+
+public class JwtCache : IAuthMemory
+{ 
+    private readonly IMemoryCache _memoryCache;
+
+    public JwtCache(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+
+    public void StoreToken(string userId, string jwt)
+    {
+        var cacheEntryOptions = new MemoryCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(30) // Set an appropriate expiration time
+        };
+
+        _memoryCache.Set(userId, jwt, cacheEntryOptions);
+    }
+
+    public string GetToken(string userId)
+    {
+        if (_memoryCache.TryGetValue(userId, out string jwt))
+        {
+            return jwt;
+        }
+
+        // Token not found in cache
+        return null;
+    }
+}
