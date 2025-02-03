915dda0dbb30003cb5f7147fb5be40cb872ed7ff
Tanaka Mawere <tanaka@tanakamawere.co.zw>
2025-02-03 22:22:10 +0200

    Blazor Server to current settings, added course selection page to components

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 using MedbaseLibrary.Services;
 using MudBlazor;
 using MudBlazor.Services;
-using Microsoft.Identity.Web;
-using MedbaseLibrary.MsalClient;
 using MedbaseBlazor;
 using System.IdentityModel.Tokens.Jwt;
-using Microsoft.Identity.Web.UI;
-using Microsoft.AspNetCore.Components.Authorization;
 using MedbaseBlazor.Pages;
+using MedbaseLibrary.CoursesAndTopics;
+using MedbaseLibrary.Essays;
+using MedbaseLibrary.Notes;
+using Medbase.Application.Clients;
+using Medbase.Application.Services;
+using Medbase.Application;
+using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,19 +21,34 @@ var environment = args.Contains("--local") ? Environments.Development : Environm
 //Dependencies
 JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
 
-builder.Services.AddScoped<IApiRepository, ApiRepository>();
-builder.Services.AddScoped<INotesRepository, NotesRepository>();
-builder.Services.AddSingleton<IPCAWrapper, PCAWrapper>();
-builder.Services.AddSingleton<IPlatformInfoService, PlatformInfoService>();
-builder.Services.AddTransient<IDatabaseRepository, DatabaseRepository>();
-builder.Services.AddTransient<ICheckForInternet, CheckForInternet>();
-builder.Services.AddScoped<AuthenticationStateProvider, MedbaseAuthenticationStateProvider>();
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+builder.Services.AddSingleton(config);
+
+// Api client registration
+builder.Services.AddHttpClient<IApiClient, ApiClient>("MedbaseApiClient", client =>
+{
+    string? baseAddress = builder.Configuration["Endpoints:Online"];
+    if (environment == Environments.Development)
+    {
+        baseAddress = builder.Configuration["Endpoints:Local"];
+    }
+
+    if (string.IsNullOrEmpty(baseAddress))
+    {
+        throw new ArgumentNullException(nameof(baseAddress), "Medbase endpoint configuration is missing or empty.");
+    }
+    client.BaseAddress = new Uri(baseAddress);
+});
+
 
 string apiString = "https://apimedbase.azurewebsites.net/";
 
 if (environment == Environments.Development)
 {
-    apiString = "http://localhost:5249/";
+    apiString = "https://apimedbase.azurewebsites.net/";
 }
 else
 {
@@ -39,16 +59,37 @@ builder.Services.AddHttpClient<IApiRepository, ApiRepository>("ApiData", client 
 {
     client.BaseAddress = new Uri(apiString);
 });
-builder.Services.AddHttpClient<INotesRepository, NotesRepository>("ApiData", client =>
-{
-    client.BaseAddress = new Uri(apiString);
-});
-builder.Services.AddHttpClient<IEssaysRepository, EssaysRepository>("ApiData", client =>
-{
-    client.BaseAddress = new Uri(apiString);
-});
-builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration, "Settings");
-builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
+
+builder.Services
+    .AddAuth0WebAppAuthentication(options => {
+        options.Domain = builder.Configuration["Auth0:Domain"];
+        options.ClientId = builder.Configuration["Auth0:ClientId"];
+    });
+
+
+//Client registration
+builder.Services.AddScoped<CoursesClient>();
+builder.Services.AddScoped<TopicsClient>();
+builder.Services.AddScoped<NotesClient>();
+builder.Services.AddScoped<EssaysClient>();
+builder.Services.AddScoped<Question2Client>();
+builder.Services.AddScoped<QuestionClient>();
+builder.Services.AddScoped<TopicsClient>();
+builder.Services.AddScoped<UserCoursePreferencesClient>();
+builder.Services.AddScoped<UserQuestionInteractionClient>();
+
+builder.Services.AddScoped<UserService>();
+
+
+//Registering services
+builder.Services.AddScoped<IApiRepository, ApiRepository>();
+builder.Services.AddScoped<INotesRepository, NotesRepository>();
+builder.Services.AddTransient<IEssaysRepository, EssaysRepository>();
+builder.Services.AddSingleton<IPlatformInfoService, PlatformInfoService>();
+builder.Services.AddTransient<IDatabaseRepository, DatabaseRepository>();
+builder.Services.AddTransient<ICheckForInternet, CheckForInternet>();
+builder.Services.AddScoped<ICoursesAndTopics, CoursesAndTopicsRepository>();
+
 builder.Services.AddAuthorization();
 builder.Services.AddAuthentication();
 builder.Services.AddCascadingAuthenticationState();
@@ -66,14 +107,35 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
-app.UseAuthentication();
-app.UseAuthorization();
+
+app.UseAntiforgery();
+
+
+app.MapGet("/Account/Login", async (HttpContext httpContext, string returnUrl = "/") =>
+{
+    var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+            .WithRedirectUri(returnUrl)
+            .Build();
+
+    await httpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+});
+
+app.MapGet("/Account/Logout", async (HttpContext httpContext) =>
+{
+    var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+            .WithRedirectUri("/")
+            .Build();
+
+    await httpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+    await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+});
 
 app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
-app.UseAntiforgery();
 
 app.UseStatusCodePagesWithRedirects("/StatusCode/{0}");
 
